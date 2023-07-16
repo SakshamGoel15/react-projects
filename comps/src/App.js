88fba2604348dfe5e16509eaf882dffcf9d33f44
SakshamGoel15 <goel.saksham99@gmail.com>
2023-07-16 17:46:09 -0400

    sixth-day

---
+++ comps/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>click!</Button>
+      </div>
+      <div>
+        <Button>click!</Button>
+      </div>
+      <div>
+        <Button>click!</Button> 
+      </div>
+      <div>
+        <Button>click!</Button>
+      </div>
+      <div>
+        <Button>click!</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
